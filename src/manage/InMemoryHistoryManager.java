c56dafe6685bdb9041ecebb2f9945a339553c90c
Vladimir <nikonoshin1999@gmail.com>
2025-09-25 21:00:06 +0400

    Исправлены ошибки согласно комментариям в pull & request

--- src/manage/InMemoryHistoryManager.java
+++ src/manage/InMemoryHistoryManager.java
@@ -9,7 +9,6 @@ import java.util.Map;
 
 public class InMemoryHistoryManager implements HistoryManager {
 
-    private final Map<Integer, Task> taskById = new HashMap<>();
     private Node head; // Начало списка
     private Node tail; // Конец списка
     private final Map<Integer, Node> nodeMap = new HashMap<>();
@@ -17,26 +16,25 @@ public class InMemoryHistoryManager implements HistoryManager {
     @Override
     public void add(Task task) {
         // Если задача уже была просмотрена, удаляем её прежнюю позицию
-        if (nodeMap.containsKey(task.getId())) {
-            Node existingNode = nodeMap.get(task.getId());
-            removeNode(existingNode); // удаляем старую позицию
-        }
-
-        // Создаем новый узел и добавляем его в конец списка
+        Node existingNode = nodeMap.get(task.getId());
+        removeNode(existingNode); // удаляем старую позицию
         Node newNode = new Node(task);
         linkLast(newNode);
         nodeMap.put(task.getId(), newNode); // Обновляем карту
     }
 
+    private List<Task> getTasks(Node node) {
+        List<Task> tasks = new ArrayList<>();
+        while (node != null) {
+            tasks.add(node.task);
+            node = node.next;
+        }
+        return tasks;
+    }
+
     @Override
     public List<Task> getHistory() {
-        List<Task> result = new ArrayList<>();
-        Node current = head;
-        while (current != null) {
-            result.add(current.task);
-            current = current.next;
-        }
-        return result;
+        return getTasks(head);
     }
 
     @Override
@@ -61,6 +59,10 @@ public class InMemoryHistoryManager implements HistoryManager {
 
     // Приватный метод для удаления узла из списка
     private void removeNode(Node node) {
+        if (node == null) {
+            return;
+        }
+
         if (node.prev != null) {
             node.prev.next = node.next;
         } else {
